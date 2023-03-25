7dd2a5a8b1e0d0bb93cccb3ceb7eb069b8b1cd6a
zwvilli <1838612269@qq.com>
2023-03-25 22:36:06 +0800

    修改package.json和umi配置

--- .umirc.ts
+++ .umirc.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   initialState: {},
   request: {},
   outputPath: 'build',
-  publicPath: 'https://github.com/zwvilli/villi/',
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://github.com/zwvilli/villi/' : '/',
   layout: {
     title: '@umijs/max',
   },
